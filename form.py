9274653a213bc32b86e4b0d2283c2eb34c441fb5
v-puppala <puppalavishal@wustl.edu>
2023-05-18 00:46:52 +0530

    register asset

--- form.py
+++ form.py
@@ -1,18 +1,58 @@
 
 import streamlit as st
-#import sqlalchemy
-#from sqlalchemy import Column, Integer, String
-#from sqlalchemy.ext.declarative import declarative_base
+import http.client
 import hashlib
+from dataclasses import dataclass
+import requests
+from pathlib import Path
+import os
+import json
+from web3 import Web3
+from pathlib import Path
+from dataclasses import dataclass
 
-#input field
 
-# Base = declarative_base()
+@dataclass
+class RealEstate:
+    name: str
+    ssn: str
+    dob: str
+    assetName: str
+    assetVal: int
 
-# class SomeClass(Base):
-#     __tablename__ = 'Clients'
-#     id = Column(Integer, primary_key=True) #this is the ssn
-#     name =  Column(String(50))
+
+name=st.sidebar.text_input("what is your name")
+
+month = None
+day = None
+year = None
+submit=False
+with st.form("dob_form"):
+        st.write("Date of Birth:")
+        day = st.number_input("Day", min_value=1, max_value=31, step=1)
+        month = st.selectbox("Month", ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"])
+        year = st.number_input("Year", min_value=1910, max_value=2010, step=1)
+        submitted = st.form_submit_button("Submit")
+        if submitted:
+            submit=True
+
+dob=f'{month}/{day}/{year}'        
+#Zillow Zestimate API CALL: Code from Rapid API: https://rapidapi.com/s.mahmoud97/api/zillow56/
+
+import requests
+
+# url = "https://zillow56.p.rapidapi.com/search"
+
+# querystring = {"location":"houston, tx","page":"3"}
+
+# headers = {
+# 	"X-RapidAPI-Key": "c0b908adb9mshf759507485b8c2fp18fda3jsn6404f42e8bd6",
+# 	"X-RapidAPI-Host": "zillow56.p.rapidapi.com"
+# }
+
+# response = requests.get(url, headers=headers, params=querystring)
+
+# print(response.json())
 
 
 ssn = st.sidebar.text_input("Enter your Social Security Number:", type="password")
@@ -25,6 +65,8 @@ if ssn:
         st.error("SSN must be a number.")
 
 ssn_again=st.sidebar.text_input("Enter your Social Security Number Again:", type="password")
+
+
 
 if ssn_again:
     if ssn!=ssn_again:
@@ -49,8 +91,35 @@ state = st.sidebar.text_input("Enter your state")
 st.write(state) 
 
 
+key=os.getenv("ALPACA_API_KEY")
+url=os.getenv("WEB3_PROVIDER_URI")
+contract_address = os.getenv("SMART_CONTRACT_ADDRESS")
+w3 = Web3(Web3.HTTPProvider(url))
+accounts=w3.eth.accounts
+account = st.selectbox("Select Wallet", options=accounts)
+
+#note make sure abi file is in same directory
+with open(Path('./TokenizedWill.json')) as f:
+    property_abi = json.load(f)
+
+
+contract = w3.eth.contract(
+        address=contract_address,
+        abi=property_abi
+)       
+
+def register():
+    hash=contract.functions.registerAsset(account,name,ssn,dob,os.getenv("assetName"),os.getenv("assetVal"), os.getenv("zillow_url")).transact({'from':account,'gas':1000000})
+    receipt = w3.eth.waitForTransactionReceipt(hash)
+    st.write("Transaction receipt mined:")
+    st.write(dict(receipt))
+    
+
+
+
 #cont=st.button("Continue", disabled="ssn" not in st.session_state)
 cont=st.button("Continue")
+#cont_fulfill=False #Boolean for whetehr or not we should fulfil request once user presses continue button
 
 
 
@@ -63,16 +132,37 @@ if len(ssn)==9 and ssn.isdigit() and ssn_again==ssn:
     st.session_state["ssn"]=ssn
     
 else:
-    st.write('make sure your social security numbers are matching, are numeric, and are 9-digits')
-cont=st.button("Continue")
-if cont and len(ssn)==9 and ssn.isdigit() and ssn_again==ssn:
-    st.write('check the error messages and fix the errors')
+    st.sidebar.write('make sure your social security numbers are matching, are numeric, and are 9-digits')
 
-@dataclass
-class RealEstate:
-    name: str
-    ssn: str
-    dob: str
-    assetName: str
-    AssetVal: int
-token=RealEstate(name="Vishal",ssn="1234567",assetName="house",dob="05/16/2023",AssetVal=5)
+if cont:
+    if len(ssn)==9 or ssn.isdigit()==True and ssn_again==ssn and name and month and day and year and address and zip and submit:
+        #cont_fulfill=True #placeholder
+        st.session_state["name"]=name
+        st.session_state["month"]=month
+        st.session_state["day"]=day
+        st.session_state["year"]=year
+        st.session_state["address"]=address
+        st.session_state["zip"]=zip
+        st.session_state["submit"]=submit
+        
+        register()
+
+            #receipt = w3.eth.waitForTransactionReceipt(hash)
+            #st.write("Transaction receipt mined:")
+            #st.write(dict(receipt))
+    
+        #st.title("af;sdjklfadjsklfjaikskjfjsd/fksa;")
+    
+        
+    else:
+        st.write('Make sure all fields are filled and filled to the specifications laid out')
+
+
+
+sampletoken=RealEstate(name="Vishal",ssn="1234567",assetName="house",dob="05/16/2023",assetVal=5)
+
+token=RealEstate(name=f'{name}',ssn=f'{ssn}',assetName="house",dob=f'{month}/{day}/{year}',assetVal=5)
+
+
+
+
