33fd4b5be9ccc605e38a5192733b4fef7b322711
v-puppala <puppalavishal@wustl.edu>
2023-05-18 01:21:35 +0530

    hard coded inputs to get successful transaction

--- form.py
+++ form.py
@@ -27,6 +27,7 @@ month = None
 day = None
 year = None
 submit=False
+dob='1/1/1910'
 with st.form("dob_form"):
         st.write("Date of Birth:")
         day = st.number_input("Day", min_value=1, max_value=31, step=1)
@@ -93,10 +94,12 @@ st.write(state)
 
 key=os.getenv("ALPACA_API_KEY")
 url=os.getenv("WEB3_PROVIDER_URI")
-contract_address = os.getenv("SMART_CONTRACT_ADDRESS")
+#contract_address = os.getenv("SMART_CONTRACT_ADDRESS")
+contract_address="0xd9145CCE52D386f254917e481eB44e9943F39138"
 w3 = Web3(Web3.HTTPProvider(url))
 accounts=w3.eth.accounts
 account = st.selectbox("Select Wallet", options=accounts)
+account=Web3.toChecksumAddress(account)
 
 #note make sure abi file is in same directory
 with open(Path('./TokenizedWill.json')) as f:
@@ -109,7 +112,7 @@ contract = w3.eth.contract(
 )       
 
 def register():
-    hash=contract.functions.registerAsset(account,name,ssn,dob,os.getenv("assetName"),os.getenv("assetVal"), os.getenv("zillow_url")).transact({'from':account,'gas':1000000})
+    hash=contract.functions.registerAsset(account,name,ssn,dob,"House",5000,"https://www.zillow.com/homedetails/12746-Roy-Rd-Brookside-Village-TX-77581/26488510_zpid/").transact({'from':account,'gas':1000000})
     receipt = w3.eth.waitForTransactionReceipt(hash)
     st.write("Transaction receipt mined:")
     st.write(dict(receipt))
