b204184f46040d8ceb38f84ef148d6ff128695b9
v-puppala <puppalavishal@wustl.edu>
2023-05-18 06:09:45 +0530

    ron

---
+++ Project 3 Ron/TokenizedWill.sol
@@ -0,0 +1,40 @@
+pragma solidity ^0.5.0;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC721/ERC721Full.sol";
+
+contract WillToken is ERC721Full {
+    constructor() public ERC721Full("RealEstToken", "RE") {}
+
+    struct RealEstate {
+        string name;
+        string ssn;
+        string dob;
+        string assetname;
+        uint256 assetvalue;
+    }
+
+    mapping(uint256 => RealEstate) public land;
+
+    event WillRecording(uint256 token_id, string name, string ssn, string assetname, uint256 assetvalue);
+
+
+    function registerAsset(
+        address owner,
+        string memory name,
+        string memory ssn,
+        string memory dob,
+        string memory assetname,
+        uint256 assetvalue,
+        string memory tokenURI
+    ) public returns (uint256) {
+        uint256 tokenID = totalSupply();
+
+        _mint(owner, tokenID);
+
+        _setTokenURI(tokenID, tokenURI);
+
+        land[tokenID] = RealEstate(name, ssn, dob, assetname, assetvalue);
+
+        return tokenID;
+    }
+}
